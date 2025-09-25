515e304a8b616008f4b5abec029e68f4b3ce8ed7
nguyenmanhhung200205-design <nguyenmanhhung200205@gmail.com>
2025-09-25 08:42:46 +0700

    Update bai3.java

--- Lab2/src/main/java/com/mycompany/lab2/bai3.java
+++ Lab2/src/main/java/com/mycompany/lab2/bai3.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,19 @@ package com.mycompany.lab2;
 public class bai3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int soDien;
+        double tien;
+
+        System.out.print("Nhap so dien su dung: ");
+        soDien = sc.nextInt();
+
+        if (soDien <= 50) {
+            tien = soDien * 1000;
+        } else {
+            tien = 50 * 1000 + (soDien - 50) * 1200;
+        }
+
+        System.out.printf("So tien dien phai tra: %.0f VND%n", tien);
     }
 }
